556aaf39caa5e10561ce178ca345bcf02dfeb138
Andriy Khomenko <andriy.khomenko@intermodaltelematics.com>
2020-08-28 12:04:56 +0100

    added splash

--- app/src/main/java/com/xomena/cmpfutboltfe/model/Pitches.kt
+++ app/src/main/java/com/xomena/cmpfutboltfe/model/Pitches.kt
@@ -2,6 +2,7 @@ package com.xomena.cmpfutboltfe.model
 
 import android.content.Context
 import android.widget.Toast
+import com.xomena.cmpfutboltfe.R
 import com.xomena.cmpfutboltfe.network.PitchesAPI
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
@@ -19,7 +20,7 @@ class Pitches @Inject constructor (private val api: PitchesAPI, private val cont
                 // And then we display it.
                 Toast.makeText(context, text as CharSequence, Toast.LENGTH_SHORT).show()
             }, {
-                it.printStackTrace()
+                Toast.makeText(context,  R.string.noData, Toast.LENGTH_LONG).show()
             })
     }
 }
